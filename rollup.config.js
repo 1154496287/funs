a89c1754567206eb41e6a508c8dcabdeda9e8586
李景龙 <lijinglong@192.168.0.112>
2022-04-30 15:31:08 +0800

    发布配置

--- rollup.config.js
+++ rollup.config.js
@@ -4,19 +4,19 @@ import babel from 'rollup-plugin-babel' // ES6转ES5
 import replace from 'rollup-plugin-replace' // 替换待打包文件里的一些变量，如process在浏览器端是不存在的，需要被替换
 import commonjs from "rollup-plugin-commonjs";
 import typescript from "rollup-plugin-typescript";
-
+import path from 'path'
 const env = process.env.NODE_ENV
 
 export default {
-	input: "tsc/lib/index.js", 
-	output: { 
-		file: 'dist/index.js', 
+	input: path.join(__dirname, '/src/index.ts'),
+	output: {
+		file: 'dist/index.js',
 		format: "umd", // umd是兼容amd/cjs/iife的通用打包格式，适合浏览器
 		name: 'file'
 	},
 	plugins: [
 		resolve(), // 查找和打包node_modules中的第三方模块
-		commonjs(), 
+		commonjs(),
 		typescript(), // 解析 TypeScript
     babel({
       exclude: 'node_modules/**' // 排除
